9b24d8942d0471b076b35fd374fc45884053d8c6
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-04 17:36:33 -0300

    Added a more robust slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,22 @@ class Category < ActiveRecord::Base
   before_save :generate_slug
 
   def generate_slug
-    self.slug = self.name.downcase
+    self.slug = self.name.parameterize
+    cat = Category.find_by(slug: self.slug)
+    count = 2
+    while cat && cat != self
+      self.slug = append_suffix(self.slug, count)
+      cat = Category.find_by(slug: self.slug)
+      count += 1
+    end
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
   end
 
   def to_param
--- app/models/post.rb
+++ app/models/post.rb
@@ -24,7 +24,22 @@ class Post < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.title.gsub(' ', '-').downcase
+    self.slug = self.title.parameterize
+    post = Post.find_by(slug: self.slug)
+    count = 2
+    while post && post != self
+      self.slug = append_suffix(self.slug, count)
+      post = Post.find_by(slug: self.slug)
+      count += 1
+    end
+  end
+
+  def append_suffix(str, count)
+    if str.split('-').last.to_i != 0
+      return str.split('-').slice(0...-1).join('-') + '-' + count.to_s
+    else
+      return str + "-" + count.to_s
+    end
   end
 
   def to_param
