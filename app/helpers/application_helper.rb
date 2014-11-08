9248aa7d7df072e655c05c41a257bbedc83390f6
Gustavo Varallo <gustavo.varallo@live.com>
2014-11-08 09:52:16 -0300

    Added follow/unfollow features

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
       "#{time_ago_in_words(dt)} ago"
     elsif logged_in? && !current_user.time_zone.blank?
       dt = dt.in_time_zone(current_user.time_zone)
-      dt.strftime("%d %b. %Y at %H:%M %Z")
+      dt.strftime("%d %b. %Y at %H:%M")
     else
       dt.strftime("%d %b. %Y at %H:%M %Z")
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :comments
   has_many :votes
+  has_many :follower_relationships, class_name: 'Relationship', foreign_key: 'leader_id'
+  has_many :leader_relationships,   class_name: 'Relationship', foreign_key: 'follower_id'
+  has_many :followers,  through: :follower_relationships, source: :follower
+  has_many :leaders  ,  through: :leader_relationships,   source: :leader
 
   has_secure_password validations: false
 
